c76a9934040be707dc58b7952cfeb08f2608eae8
leanhhuy103161 <leanhhuy103161@gmail.com>
2021-06-03 10:19:58 +0700

    create simple restful api for user

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,8 +1,11 @@
 package com.example.demo.services;
 
+import com.example.demo.entity.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService {
-
+    public List<User> getListUser();
 }
--- src/main/java/com/example/demo/services/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/UserServiceImpl.java
@@ -4,12 +4,37 @@ import com.example.demo.entity.User;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UserServiceImpl implements UserService {
     private static ArrayList<User> users = new ArrayList<User>();
 
     static {
-        users.add(new User(1, "The Last Jedi", "jedi13@gmail.com", "0898213745", "https://source.unsplash.com/random", "thelastjedi"));
+        users.add(new User(1,
+                "The Last Jedi",
+                "jedi13@gmail.com",
+                "0898213745",
+                "https://source.unsplash.com/random",
+                "thelastjedi"));
+
+        users.add(new User(2,
+                "Han Solo",
+                "hansolo10@gmail.com",
+                "0898213745",
+                "https://source.unsplash.com/random",
+                "hansolo"));
+
+        users.add(new User(3,
+                "The Mandalorian",
+                "mandalorian1000@gmail.com",
+                "0898213745",
+                "https://source.unsplash.com/random",
+                "thisistheway"));
+    }
+
+    @Override
+    public List<User> getListUser() {
+        return null;
     }
 }
